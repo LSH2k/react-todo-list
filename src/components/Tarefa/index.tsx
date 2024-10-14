27a69559507965a91f3b1617415b766f8536314a
Leandro Sousa <156814218+LSH2k@users.noreply.github.com>
2024-10-14 10:18:58 -0300

    Initial commit

---
+++ src/components/Tarefa/index.tsx
@@ -0,0 +1,101 @@
+import { ChangeEvent, useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux'
+
+import * as S from './styles'
+import { Botao, BotaoSalvar } from '../../styles'
+
+import { remover, editar, alterarStatus } from '../../store/reducers/tarefas'
+import TarefaClass from '../../models/Tarefa'
+
+import * as enums from '../../utils/enums/Tarefa'
+
+type Props = TarefaClass
+
+const Tarefa = ({
+  descricao: descricaoOriginal,
+  prioridade,
+  status,
+  titulo,
+  id
+}: Props) => {
+  const dispatch = useDispatch()
+  const [estaEditando, setEstaEditando] = useState(false)
+  const [descricao, setDescricao] = useState('')
+
+  useEffect(() => {
+    if (descricaoOriginal.length > 0) {
+      setDescricao(descricaoOriginal)
+    }
+  }, [descricaoOriginal])
+
+  function cancelarEdicao() {
+    setEstaEditando(false)
+    setDescricao(descricaoOriginal)
+  }
+
+  function alteraStatusTarefa(e: ChangeEvent<HTMLInputElement>) {
+    dispatch(alterarStatus({ id, finalizado: e.target.checked }))
+  }
+
+  return (
+    <S.Card>
+      <label htmlFor={titulo}>
+        <input
+          type="checkbox"
+          checked={status === enums.Status.CONCLUIDA}
+          onChange={alteraStatusTarefa}
+          id={titulo}
+        />
+        <S.Titulo>
+          {estaEditando && <em>Editando: </em>}
+          {titulo}
+        </S.Titulo>
+      </label>
+      <S.Tag parametro="prioridade" prioridade={prioridade}>
+        {prioridade}
+      </S.Tag>
+      <S.Tag parametro="status" status={status}>
+        {status}
+      </S.Tag>
+      <S.Descricao
+        disabled={!estaEditando}
+        value={descricao}
+        onChange={(e) => setDescricao(e.target.value)}
+      ></S.Descricao>
+      <S.BarraAcoes>
+        {estaEditando ? (
+          <>
+            <BotaoSalvar
+              onClick={() => {
+                dispatch(
+                  editar({
+                    descricao,
+                    prioridade,
+                    status,
+                    titulo,
+                    id
+                  })
+                )
+                setEstaEditando(false)
+              }}
+            >
+              Salvar
+            </BotaoSalvar>
+            <S.BotaoCancelarRemover onClick={cancelarEdicao}>
+              Cancelar
+            </S.BotaoCancelarRemover>
+          </>
+        ) : (
+          <>
+            <Botao onClick={() => setEstaEditando(true)}>Editar</Botao>
+            <S.BotaoCancelarRemover onClick={() => dispatch(remover(id))}>
+              Remover
+            </S.BotaoCancelarRemover>
+          </>
+        )}
+      </S.BarraAcoes>
+    </S.Card>
+  )
+}
+
+export default Tarefa
---
+++ src/store/reducers/tarefas.ts
@@ -0,0 +1,87 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import Tarefa from '../../models/Tarefa'
+import * as enums from '../../utils/enums/Tarefa'
+
+type TarefasState = {
+  itens: Tarefa[]
+}
+
+const initialState: TarefasState = {
+  itens: [
+    {
+      id: 1,
+      descricao: 'Estudar modulo 7',
+      prioridade: enums.Prioridade.NORMAL,
+      status: enums.Status.CONCLUIDA,
+      titulo: 'Estudar JS'
+    },
+    {
+      id: 2,
+      descricao: 'Estudar material de apoio',
+      prioridade: enums.Prioridade.NORMAL,
+      status: enums.Status.PENDENTE,
+      titulo: 'Estudar TS'
+    },
+    {
+      id: 3,
+      descricao: 'Estudar landing pages',
+      prioridade: enums.Prioridade.IMPORTANTE,
+      status: enums.Status.PENDENTE,
+      titulo: 'Estudar BS'
+    }
+  ]
+}
+
+const tarefasSlice = createSlice({
+  name: 'tarefas',
+  initialState,
+  reducers: {
+    remover: (state, action: PayloadAction<number>) => {
+      state.itens = [
+        ...state.itens.filter((tarefa) => tarefa.id !== action.payload)
+      ]
+    },
+    editar: (state, action: PayloadAction<Tarefa>) => {
+      const indexTarefa = state.itens.findIndex(
+        (t) => t.id === action.payload.id
+      )
+      if (indexTarefa >= 0) {
+        state.itens[indexTarefa] = action.payload
+      }
+    },
+    cadastrar: (state, action: PayloadAction<Omit<Tarefa, 'id'>>) => {
+      const tarefaJaExiste = state.itens.find(
+        (tarefa) =>
+          tarefa.titulo.toLowerCase() === action.payload.titulo.toLowerCase()
+      )
+      if (tarefaJaExiste) {
+        alert(`Já existe uma tarefa com esse nome`)
+      } else {
+        const ultimaTarefa = state.itens[state.itens.length - 1]
+        const tarefaNova = {
+          ...action.payload,
+          id: ultimaTarefa ? ultimaTarefa.id + 1 : 1
+        }
+        state.itens.push(tarefaNova)
+      }
+    },
+    alterarStatus: (
+      state,
+      action: PayloadAction<{ id: number; finalizado: boolean }>
+    ) => {
+      const indexTarefa = state.itens.findIndex(
+        (t) => t.id === action.payload.id
+      )
+      if (indexTarefa >= 0) {
+        state.itens[indexTarefa].status = action.payload.finalizado
+          ? enums.Status.CONCLUIDA
+          : enums.Status.PENDENTE
+      }
+    }
+  }
+})
+
+export const { remover, editar, cadastrar, alterarStatus } =
+  tarefasSlice.actions
+
+export default tarefasSlice.reducer
